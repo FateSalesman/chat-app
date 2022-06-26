b3a688e0ce77c91c7823cd3d5a7b9ff302d955d2
Pranav Mallina <pranavseshu1834@gmail.com>
2022-06-26 17:17:18 +0530

    Revert "Update chatroom.js"
    This reverts commit 056e8b299040bb32445fc208668ae75bab4182f9.

--- chat/src/components/ChatRoom/chatroom.js
+++ chat/src/components/ChatRoom/chatroom.js
@@ -1,13 +1,9 @@
 import React from "react";
 import { signOut } from 'firebase/auth'
 import { useCollectionData } from "react-firebase-hooks/firestore";
-import { collection } from 'firebase/firestore'
+
 
 export default function ChatRoom(props) {
-
-    const messageRef = collection('messages');
-    const query = messageRef.orderBy('timestamp').limit(25);
-
     return (
         <div className="ChatRoom">
             <p>Hello!</p>
